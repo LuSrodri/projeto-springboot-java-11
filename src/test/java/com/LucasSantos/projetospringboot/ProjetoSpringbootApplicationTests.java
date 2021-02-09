f3da348541e16acb3a275a1d044bc627967cf2be
LuSrodri <lucassrodri08@gmail.com>
2021-02-09 13:11:09 -0300

    Projeto Criado

---
+++ src/test/java/com/LucasSantos/projetospringboot/ProjetoSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LucasSantos.projetospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
