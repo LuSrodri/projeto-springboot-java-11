934bd211a34a101f5f5199d4b2ce3f1c41fd5954
LuSrodri <lucassrodri08@gmail.com>
2021-02-10 15:52:30 -0300

    Order, Instant, ISO 8601 - Parte 2

---
+++ src/main/java/com/LucasSantos/projetospringboot/repositories/OrderRepository.java
@@ -0,0 +1,11 @@
+package com.LucasSantos.projetospringboot.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.LucasSantos.projetospringboot.entities.Order;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+	
+}
