b4102eebb3320e4e4c774083d0d358127f146664
LuSrodri <lucassrodri08@gmail.com>
2021-02-14 12:54:42 -0300

    Payment, one-to-one association

--- src/main/java/com/LucasSantos/projetospringboot/config/TestConfig.java
+++ src/main/java/com/LucasSantos/projetospringboot/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.LucasSantos.projetospringboot.entities.Category;
 import com.LucasSantos.projetospringboot.entities.Order;
 import com.LucasSantos.projetospringboot.entities.OrderItem;
+import com.LucasSantos.projetospringboot.entities.Payment;
 import com.LucasSantos.projetospringboot.entities.Product;
 import com.LucasSantos.projetospringboot.entities.User;
 import com.LucasSantos.projetospringboot.entities.enums.OrderStatus;
@@ -80,6 +81,12 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
+		
 	}
 	
 	
